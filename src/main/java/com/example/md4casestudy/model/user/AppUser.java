b2a3c5b0e9772a6423fc1e524d1b755be3cc5f2e
GiangVB <giangvb192@gmaiil.com>
2022-05-12 15:03:29 +0700

    edit AppUser

--- src/main/java/com/example/md4casestudy/model/user/AppUser.java
+++ src/main/java/com/example/md4casestudy/model/user/AppUser.java
@@ -7,8 +7,14 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String user_name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
     @ManyToOne
